7232e034de4e46591f0dd11e2357263d870852aa
Chris <chris.ko791@gmail.com>
2018-02-23 21:44:25 -0500

    Initial spring boot starter commit

---
+++ src/main/java/com/securecouch/pridehack/service/SuggestionsApiApplication.java
@@ -0,0 +1,12 @@
+package com.securecouch.pridehack.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuggestionsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuggestionsApiApplication.class, args);
+	}
+}
